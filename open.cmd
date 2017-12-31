d4c35ff900fa2795bb7162a2a4315b084ae13201
florian dahlitz <f2dahlitz@freenet.de>
2017-12-31 22:55:19 +0100

    show available keys using -s flag

--- open.cmd
+++ open.cmd
@@ -1,6 +1,9 @@
 @ECHO OFF
+
+if "%1"=="-s" GOTO ShowKeys
 set params=
 set k=0
+
 :Loop
 IF "%1"=="" GOTO Continue
 IF %k%==0 ( 
@@ -9,8 +12,24 @@ IF %k%==0 (
     GOTO AfterFirst
     )
 set params=%params% %1
+
 :AfterFirst
 SHIFT
 GOTO Loop
+
 :Continue
 start "" "open_helper.py" %params%
+GOTO End
+
+:ShowKeys
+@ECHO ----------
+FOR /F "tokens=* delims= " %%i in (programs.txt) DO (
+    set line=%%i
+    call :processToken
+    )
+@ECHO ----------
+
+:processToken
+    FOR /F "tokens=1* delims=-" %%j in ("%line%") DO @ECHO %%j
+
+:End
